19f7bd66369176aa1dcb163d80f3a84bcf6a3df6
Amy Mangor <aemangor@gmail.com>
2021-05-21 15:32:04 -0400

    commit tree 121fd91ec127948f5b0edb431e891da31e05e09c

---
+++ lib/backer.rb
@@ -0,0 +1,27 @@
+
+
+class Backer
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    
+    end
+
+def back_project(project)
+ProjectBacker.new(project, self)
+end
+
+def backed_projects
+    result = ProjectBacker.all.select do |prBacker|
+          prBacker.backer == self
+      end
+      result.map do |t|
+          t.project
+      end
+
+  # binding.pry
+  end
+
+end
